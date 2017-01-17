fccc1440d04e50b6defa0b8477cc323383daf1b5
bogdanviktor <bogdanviktor@gmail.com>
2017-01-17 14:00:26 +0000

    sync

--- assets/js/order.js
+++ assets/js/order.js
@@ -1,4 +1,5 @@
 function goToOrder() {
    var param=$('input:radio[name ="optionsRadios"]:checked').val();
-   window.location.href="https://order.sendyojee.com/?type="+param;
+   var order="https://order.sendyojee.com/?type="+param;
+   window.open(order);
 }
